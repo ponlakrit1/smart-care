a01e4bda54e2e02f2c3da294d3f78944e6d7ab6a
Nctine <ponlakritkm@gmail.com>
2019-02-26 20:35:40 +0700

    add spinner for amount of foods

--- app/src/main/java/com/example/smartcare/ConsumptionActivity.java
+++ app/src/main/java/com/example/smartcare/ConsumptionActivity.java
@@ -4,10 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class ConsumptionActivity extends AppCompatActivity {
+
+    private SeekBar sbVolume;
+
+    TextView fCal;
+    TextView fName;
+    TextView foodAmountText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +25,31 @@ public class ConsumptionActivity extends AppCompatActivity {
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         this.getSupportActionBar().setHomeButtonEnabled(true);
 
+        fName = (TextView) findViewById(R.id.foodName);
+        fCal = (TextView) findViewById(R.id.foodCalNum);
+        foodAmountText = (TextView) findViewById(R.id.foodAmount);
+
+        sbVolume = (SeekBar) findViewById(R.id.seekBar);
+        sbVolume.setMax(10);
+        sbVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                foodAmountText.setText("จำนวน "+progress);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
         Intent intent = getIntent();
         String foodName = intent.getStringExtra("foodName");
         String foodCal = intent.getStringExtra("foodCal");
 
-        TextView fName = (TextView) findViewById(R.id.foodName);
-        TextView fCal = (TextView) findViewById(R.id.foodCalNum);
-
         fName.setText(foodName);
-        fCal.setText(foodCal+" calories");
+        fCal.setText(foodCal+" แคลอรี่");
     }
 
     @Override
